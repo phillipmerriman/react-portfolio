8565c6dda8d2ad91a6117af5a14d0be9e85d9e18
Phillip Merriman <phillipmerriman@gmail.com>
2021-03-07 17:06:35 -0600

    feat: home page styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,21 @@
 import React from "react";
+import image from "../conifer-1867371_1920.jpg"
 
 export default function Home () {
-    return <h1>Home Page!!!!!</h1>
+    return (
+        <main>
+            <img 
+                src={image} 
+                alt="Conifer Leaves" 
+                className="
+                absolute 
+                object-cover 
+                w-full 
+                h-full"
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hola! I'm Phillip.</h1>
+            </section>
+        </main>
+    )
 }
